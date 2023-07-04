d2b9c5e45fab5fd1ba4e6fabed2ca6e850822c88
Angel Gaitan <133923765+AngelGaitan@users.noreply.github.com>
2023-07-04 09:27:16 -0600

    no cambios

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return(
+        <>
+        <nav className="navbar">
+          <div className="container">
+            <h3>Predicción metereologica</h3>
+            
+            </div>  
+        </nav>
+        </>
+    )
+}
+
+export default NavBar
